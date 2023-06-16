2acf9596d302d4977a68b9a8dbb3bdcfc1b9574a
sami.benlaribi <sami.benlaribi@esprit.tn>
2023-06-16 19:12:57 +0100

    first commit

---
+++ src/main/java/com/sbl/microservice_project/api_gateway/ApiGatewayApplication.java
@@ -0,0 +1,18 @@
+package com.sbl.microservice_project.api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import com.sbl.microservice_project.api_gateway.ApiGatewayApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+}
